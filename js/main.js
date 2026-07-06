d2df2d3e99aeea96c67b3f88378e1ba784c17c4c
davifelipef <feliperedfield@gmail.com>
2026-07-06 15:18:49 -0300

    chore: restored the original main.js for bug investigation

--- js/main.js
+++ js/main.js
@@ -64,29 +64,18 @@ class Main {
     }
 
     loadMainScripts() {
-        /* STREAMING_CHUNK:Iniciando carregamento sequencial de scripts */
-        // Carregamento sequencial para garantir que os scripts core carreguem antes do setup
-        let index = 0;
-        const loadNext = () => {
-            if (index < scriptUrls.length) {
-                const script = document.createElement("script");
-                script.src = scriptUrls[index];
-                script.onload = () => {
-                    index++;
-                    loadNext();
-                };
-                script.onerror = this.onScriptError.bind(this);
-                script._url = scriptUrls[index];
-                document.body.appendChild(script);
-            } else {
-                /* STREAMING_CHUNK:Configurando plugins e inicializando jogo */
-                // A inicialização do PluginManager deve ocorrer apenas após o carregamento total
-                PluginManager.setup($plugins);
-                // Garantir que a inicialização do jogo ocorra após o setup dos plugins
-                this.onAllScriptsLoaded();
-            }
-        };
-        loadNext();
+        for (const url of scriptUrls) {
+            const script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = url;
+            script.async = false;
+            script.defer = true;
+            script.onload = this.onScriptLoad.bind(this);
+            script.onerror = this.onScriptError.bind(this);
+            script._url = url;
+            document.body.appendChild(script);
+        }
+        this.numScripts = scriptUrls.length;
         window.addEventListener("load", this.onWindowLoad.bind(this));
         window.addEventListener("error", this.onWindowError.bind(this));
     }
